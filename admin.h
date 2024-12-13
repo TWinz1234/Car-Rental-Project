c0dd6ff3c3a2e3df8b6e57fd2725f6bbe166f818
KevinEinWindir <86865356+KevinEinWindir@users.noreply.github.com>
2024-12-12 22:55:28 -0500

    Update admin.h
    semi-final

--- admin.h
+++ admin.h
@@ -1,25 +1,24 @@
 #ifndef ADMIN_H
 #define ADMIN_H
-
 #include <string>
-#include <vector>
-#include <memory>
 #include "BaseCar.h"
+using namespace std;
 
 // I think this class needs to be modified to make the classes private
 // it still works as it is, the logical is just not perfect
 class Admin {
 private:
-    std::vector<std::shared_ptr<BaseCar>> inventory; // Private inventory
-    const std::string adminPassword = "admin123";    // Admin password
+    // Private inventory & admin password
+    vector<shared_ptr<BaseCar>> inventory;
+    const string adminPassword = "admin123";
 
 public:
     Admin();
-    bool authenticate(const std::string& password) const;
-    void addCar(const std::shared_ptr<BaseCar>& car);
+    bool authenticate(const string& password) const;
+    void addCar(const shared_ptr<BaseCar>& car);
     void listInventory() const;
     void processPayment(double amount) const;
-    void setInventory(std::vector<std::shared_ptr<BaseCar>> inv);
+    void setInventory(vector<shared_ptr<BaseCar>> inv);
 };
 
 #endif
